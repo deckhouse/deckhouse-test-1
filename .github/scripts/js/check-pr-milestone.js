883be1842dfe8e160fe0342a8ce7cbb9bd0a4c53
Artem Kladov <artem.kladov@flant.com>
2022-09-28 14:06:25 +0300

    +++
    Signed-off-by: Artem Kladov <artem.kladov@flant.com>

---
+++ .github/scripts/js/check-pr-milestone.js
@@ -0,0 +1,19 @@
+
+/**
+ * Start 'build-and-test_release.yml' workflow depending on context.ref.
+ *
+ * @param {object} inputs
+ * @param {object} inputs.github - A pre-authenticated octokit/rest.js client with pagination plugins.
+ * @param {object} inputs.context - An object containing the context of the workflow run.
+ * @param {object} inputs.core - A reference to the '@actions/core' package.
+ * @returns {Promise<void>}
+ */
+module.exports.checkPrMilestone = async ({ github, context, core }) => {
+    const pr = context.payload.pull_request;
+
+    if (pr.milestone) {
+      core.debug(`This pull request has a milestone: ${pr.milestone.title}`);
+    } else {
+      core.setFailed("The pull request has no milestone. Set a milestone for the pull request.");
+    }
+}
