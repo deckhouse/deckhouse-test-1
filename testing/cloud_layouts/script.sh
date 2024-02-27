4aa4a03ba5b18c40d9d172c488f4f66625a53a06
Nikita Korolev <nikita.korolev@flant.com>
2024-02-27 23:06:39 +0300

    chore: fix master_ip output; change regexp
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>
    
    chore: fix master_ip output; change regexp
    
    Signed-off-by: Nikita Korolev <nikita.korolev@flant.com>

--- testing/cloud_layouts/script.sh
+++ testing/cloud_layouts/script.sh
@@ -846,10 +846,11 @@ END_SCRIPT
 function parse_master_ip_from_log() {
   # local ip
   >&2 echo "  Detect master_ip from bootstrap.log ..."
-  # if ! master_ip="$(grep -Po '(?<=master_ip_address_for_ssh = ).+$' "$bootstrap_log")"; then
+  # if ! master_ip="$(grep -Po '(?<=master_ip_address_for_ssh = ).+(?=")$' "$bootstrap_log")"; then
+  if ! master_ip="$(grep -Po '(?<=master_ip_address_for_ssh = ")((\d{1,3}\.){3}\d{1,3})(?=")' "$bootstrap_log")"; then
   # cut -d "=" -f2 | tr -d "\" ")"
   # "$(grep "master_ip_address_for_ssh" "$cwd/terraform.log"| cut -d "=" -f2 | tr -d "\" ")"
-  if ! master_ip="$(grep "master_ip_address_for_ssh" "$bootstrap_log"| cut -d "=" -f2 | tr -d "\" ")"; then
+  # if ! master_ip="$(grep "master_ip_address_for_ssh" "$bootstrap_log"| cut -d "=" -f2 | tr -d "\" ")"; then
     >&2 echo "    ERROR: can't parse master_ip from bootstrap.log"
     return 1
   fi
